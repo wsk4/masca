9ac724142c3b8c9a852ceb95cd233bf2bdc75a79
Veliz1232 <cr.veliz@duocuc.cl>
2025-10-09 20:54:35 -0300

    blogs"terminado"

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import NotFound from './pages/NotFound';
 import Contact from './pages/Contact';
 import Blogs from './pages/Blogs';
 import Nosotros from './pages/Nosotros';
+import BlogDetail from './pages/BlogsDetail';
 
 function App() {
   return (
@@ -19,8 +20,11 @@ function App() {
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:id" element={<BlogDetail />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
     </>
   );
---
+++ src/components/organisms/BlogsCard.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Col, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Button from "../atoms/Button";
+
+
+function BlogCard({ blog }) {
+  const navigate = useNavigate();
+
+  return (
+    <Col md={6} className="mb-4">
+      <Card
+        className="text-light"
+        style={{
+          background: "linear-gradient(135deg, #a200ff, #0072ff)",
+          border: "none",
+          borderRadius: "10px",
+          padding: "1.5rem",
+          boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
+        }}
+      >
+        <Card.Body>
+          <Card.Title className="text-center">{blog.title}</Card.Title>
+          <Card.Text>{blog.summary}</Card.Text>
+          <div className="text-center">
+            <Button
+              variant="primary"
+              onClick={() => navigate(`/blogs/${blog.id}`)}
+              style={{
+                background: "linear-gradient(135deg, #a200ff, #0072ff)",
+                border: "2px solid black",
+              }}
+            >
+              Ver caso
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default BlogCard;
