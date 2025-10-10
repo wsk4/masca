a29639ec9937ccce2cb0b9ffcbe2aa1c98b4ba1d
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-09 22:12:07 -0300

    67

--- src/pages/BlogsDetail.jsx
+++ src/pages/BlogsDetail.jsx
@@ -1,24 +1,61 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import BlogCard from "../components/organisms/BlogsCard";
+import { Container, Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/atoms/Button";
+import Image from "../components/atoms/Image";
+import Text from "../components/atoms/Text";
 import blogs from "../data/Blogs";
+import  "../styles/blogs.css";
 
-function Blogs() {
+
+function BlogDetail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const blog = blogs.find((b) => b.id === parseInt(id));
+
+  if (!blog) {
+    return (
+      <Container className="my-5 text-center">
+        <h1>Caso no encontrado</h1>
+        <Button variant="primary" onClick={() => navigate("/blogs")}>
+          Volver
+        </Button>
+      </Container>
+    );
+  }
+
   return (
     <Container className="my-5">
-      <h1 className="text-dark fw-bold text-center">Casos curiosos</h1>
-      <p className="text-center mb-4 text-dark">
-        En esta sección encontrarás algunas experiencias divertidas y reales de
-        nuestros clientes. ¡Porque en Mascapitos Store cada aroma tiene una
-        historia!
-      </p>
-      <Row>
-        {blogs.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </Row>
+      <Button variant="primary" onClick={() => navigate("/blogs")}>
+        Volver
+      </Button>
+      <Card
+        className="text-light mt-3"
+        style={{
+          background: "linear-gradient(135deg, #a200ff, #0072ff)",
+          border: "none",
+          borderRadius: "10px",
+          padding: "1.5rem",
+          boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
+        }}
+      >
+        <Card.Body>
+          <Text variant="h2" className="text-center mb-4">
+            {blog.tittle}
+          </Text>
+          <Text variant="p">{blog.content}</Text>
+          <div className="text-center mt-4">
+            <Image
+              src={blog.image}
+              alt={blog.tittle}
+              className="blog-detail-image"
+            />
+          </div>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
 
-export default Blogs;
+export default BlogDetail;
