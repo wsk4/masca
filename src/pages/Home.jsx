c305900327f6c2a8c04dc451708a3c079db4feb9
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-10 22:13:44 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,17 @@ function Home() {
     <Container className="my-5">
       <h1>Página de Inicio</h1>
       <p>Bienvenidos a nuestro sitio web.</p>
+      <div class="section grey lighten-3">
+        <div class="container row valign-wrapper">
+          <div class="col s12 m6">
+            <h4>Mascapitos Store</h4>
+            <p>
+              Nuestra tienda online ofrece productos seleccionados de todas las regiones. 
+              Encuentra lo que necesitas al mejor precio.
+            </p>
+          </div>
+        </div>
+    </div>
     </Container>
     
   );
