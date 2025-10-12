eaa0544f3baea7560717fc4d95492a65a13af4ce
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-12 19:34:25 -0300

    Update Productos.js

--- src/data/Productos.js
+++ src/data/Productos.js
@@ -4,29 +4,29 @@ const productos = [
         nombre: "Phantom",
         marca: "Paco Rabanne",
         precio: 72000,
-        imagen: "./img/phantome_11zon.webp", 
+            imagen: "./img/phantome_11zon.webp", 
     },
     {
         id: 2,
         nombre: "Valentino",
         marca: "Valentino",
         precio: 85000,
-        imagen: "./img/valentino.webp",
+            imagen: "./img/valentino.webp",
     },
     {
         id: 3,
         nombre: "Devoción",
         marca: "Antonio Banderas",
         precio: 49000,
-        imagen: "./img/devocion.webp",
+            imagen: "./img/devocion.webp",
     },
     {
         id: 4,
         nombre: "Egeo Dolce",
         marca: "O Boticário",
         precio: 67000,
-        imagen: "./img/perfume.webp",
-    },
+            imagen: "./img/perfume.webp",
+    }
 ];
 
 export default productos;
