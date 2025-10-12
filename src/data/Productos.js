3823e5c087217206c8aefc69c952cda766d2e1f2
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-12 20:41:21 -0300

    .

--- src/data/Productos.js
+++ src/data/Productos.js
@@ -4,28 +4,28 @@ const productos = [
         nombre: "Phantom",
         marca: "Paco Rabanne",
         precio: 120000,
-            image: "/img/phantome_11zon.webp", 
+            image: "./img/phantome_11zon.webp", 
     },
     {
         id: 2,
         nombre: "Valentino",
         marca: "Valentino",
         precio: 100000,
-            image: "/img/valentino.webp",
+            image: "./img/valentino.webp",
     },
     {
         id: 3,
         nombre: "Devoción",
         marca: "Antonio Banderas",
         precio: 95000,
-            image: "/img/devocion.webp",
+            image: "./img/devocion.webp",
     },
     {
         id: 4,
         nombre: "Stronger with you",
         marca: "Emporio Armani",
         precio: 110000,
-            image: "/img/perfume.webp",
+            image: "./img/perfume.webp",
     }
 ];
 
