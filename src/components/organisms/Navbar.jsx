85dbca366fe675f954a67990a61d14e60ece3978
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-12 22:35:32 -0300

    Update Navbar.jsx

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+// 1. Importa 'Link' desde React Router
+import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function NavBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Mascapitos Store</Navbar.Brand>
+        {/* 2. Cambia href por 'to' y usa 'as={Link}' */}
+        <Navbar.Brand as={Link} to="/">Mascapitos Store</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/products">Productos</Nav.Link>
-            <Nav.Link href="/contact">Contacto</Nav.Link>
-            <Nav.Link href="/blogs">Blogs</Nav.Link>
-            <Nav.Link href="/nosotros">Nosotros</Nav.Link>
-            <Nav.Link href="/login">Inicio sesion</Nav.Link>
-            <Nav.Link href="/crearUsuario">Registro</Nav.Link>
-            <Nav.Link href="/cart">Carrito</Nav.Link>
+            {/* 3. Aplica el mismo cambio a todos los enlaces */}
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/products">Productos</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+            <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
+            <Nav.Link as={Link} to="/login">Inicio sesion</Nav.Link>
+            <Nav.Link as={Link} to="/crearUsuario">Registro</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Carrito</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
