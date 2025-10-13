ce70f78cf27e70b4908e7ca1703a3a4a74a8e1bc
wsk4 <maa.gonzalezva@duocuc.cl>
2025-10-12 21:54:22 -0300

    momoerror

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import Image from '../components/atoms/Image';
 
 const image = {
-    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnLMSjfk8BbBLwEAf2T3zSpK8hhg8bMkHPww&s',
+    src: '/img/momoerror.webp',
     alt: 'Not Found Image',
 }
 
