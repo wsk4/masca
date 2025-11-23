f33e3e62e8a5fcbfe969376d46de7c835ac245ff
Moonlighpoio <ren.barriga@duocuc.cl>
2025-11-23 16:41:39 -0300

    mejora color botones

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -6,18 +6,19 @@ function Button({ text, onClick, className = "", disabled = false, ...props }) {
 	return (
 		<button onClick={onClick} disabled={disabled} 
 			className={`
-                /* CLASES BASE QUE DAN LA FORMA */
+                /* Estilos base (forma, padding, etc.) */
 				px-5 py-2.5 rounded-lg font-bold text-sm transition-all duration-200
+                border border-transparent
 				focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-theme-text focus:ring-offset-theme-main
                 
-                /* PRIORIZA LA CLASE QUE VIENE POR PROPS */
-				${className} 
-                
-                /* ESTILOS PREDETERMINADOS (que pueden ser sobrescritos por className) */
+                /* Estilos de interacción */
 				${disabled 
                     ? 'opacity-70 cursor-not-allowed' 
                     : 'hover:scale-105 active:scale-95 shadow-[0_0_15px_rgba(255,255,255,0.15)]'
                 }
+                
+                /* INYECTAR LA CLASE PASADA POR PROPS AL FINAL (PRIORIDAD) */
+                ${className} 
             `} 
             {...props} 
         >
