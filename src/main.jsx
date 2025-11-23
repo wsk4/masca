0a17fa5a71dee5368898d904d972ab2ba9dd6527
Moonlighpoio <ren.barriga@duocuc.cl>
2025-11-23 18:22:49 -0300

    xd

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext'; 
 import './index.css';
 import App from './App.jsx';
 
@@ -9,7 +11,9 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <App />
+                <CartProvider> 
+                    <App />
+                </CartProvider>
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>
